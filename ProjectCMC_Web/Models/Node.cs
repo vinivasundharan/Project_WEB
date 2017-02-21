8961b23bc880dbe70b5f2ddc95948f08e2af5120
Danny_FH <reachoutdan@gmail.com>
2017-02-21 10:50:12 +0100

    test

---
+++ ProjectCMC_Web/Models/Node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectCMC_Web.Models
+{
+    public class Node
+    {
+        public int NodeID { get; set; }
+        [Display(Name = "Node Name")]
+        public string NodeName { get; set; }
+        [Display(Name = "Node Description")]
+        public DateTime NodeDescription { get; set; }
+        [Display(Name = "Created On")]
+        public DateTime CreatedOn { get; set; }
+        [Display(Name = "Modified On")]
+        public DateTime ModifiedOn { get; set; }
+        [Display(Name = "Created By")]
+        public string CreatedBy { get; set; }
+        [Display(Name = "Modified By")]
+        public string ModifiedBy { get; set; }
+
+        public int WindMillID { get; set; }
+        public WindMill WindMill { get; set; }
+        // 23456456
+    }
+}
