659368b39311b5633bcd25db754dc184e764a560
Danny_FH <reachoutdan@gmail.com>
2017-02-21 11:07:23 +0100

    Node as FK added in Wind Mill

--- ProjectCMC_Web/Models/WindMill.cs
+++ ProjectCMC_Web/Models/WindMill.cs
@@ -22,10 +22,11 @@ namespace ProjectCMC_Web.Models
         public string ManufacturerName { get; set; }
         public int WindParkID { get; set; }
         public WindPark Windpark { get; set; }
-        //sdgsdgdgdgb nmghm ffbgjm  fghf dfgdh
         public int LocationID { get; set; }
         public Location Location { get; set; }
+        //Foriegn Key for Node
+        public List<Node> Node { get; set; }
 
-         
+
     }
 }
