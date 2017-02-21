4870a6d523a6b326b7dcf7b00df8f81b2187b47a
vini vasundharan <vinivasundharan@outlook.com>
2017-02-21 09:39:24 +0100

    First commit

---
+++ ProjectCMC_Web/Models/WindMill.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace ProjectCMC_Web.Models
+{
+    public class WindMill
+    {
+        public int WindMillID { get; set; }
+        [Display(Name = "Wind Park Name")]
+        public string WindMillName { get; set; }
+        [Display(Name = "Created On")]
+        public DateTime CreatedOn { get; set; }
+        [Display(Name = "Modified On")]
+        public DateTime ModifiedOn { get; set; }
+        [Display(Name = "Created By")]
+        public string CreatedBy { get; set; }
+        [Display(Name = "Modified By")]
+        public string ModifiedBy { get; set; }
+
+        public int WindParkID { get; set; }
+        public WindPark Windpark { get; set; }
+
+    }
+}
---
+++ ProjectCMC_Web/Models/WindPark.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectCMC_Web.Models
+{
+    public class WindPark
+    {
+        public int WindParkID { get; set; }
+        [Display(Name="Wind Park Name")]
+        public string WindParkName { get; set; }
+        [Display(Name = "Created On")]
+        public DateTime CreatedOn { get; set; }
+        [Display(Name = "Modified On")]
+        public DateTime ModifiedOn { get; set; }
+        [Display(Name = "Created By")]
+        public string CreatedBy { get; set; }
+        [Display(Name = "Modified By")]
+        public string ModifiedBy { get; set; }
+
+        public List<WindMill> WindMills { get; set; }
+    }
+}
