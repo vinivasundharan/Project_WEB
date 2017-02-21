dd3cf4cd6ebd2c3d53e7060f9adc8c85fdbac708
vini vasundharan <vinivasundharan@outlook.com>
2017-02-21 10:45:43 +0100

    Model Update

--- ProjectCMC_Web/Models/WindMill.cs
+++ ProjectCMC_Web/Models/WindMill.cs
@@ -18,10 +18,14 @@ namespace ProjectCMC_Web.Models
         public string CreatedBy { get; set; }
         [Display(Name = "Modified By")]
         public string ModifiedBy { get; set; }
-
+        [Display(Name = "Manufacturer Name")]
+        public string ManufacturerName { get; set; }
         public int WindParkID { get; set; }
         public WindPark Windpark { get; set; }
         //sdgsdgdgdgb nmghm ffbgjm  fghf dfgdh
+        public int LocationID { get; set; }
+        public Location Location { get; set; }
+
          
     }
 }
